cb3b9d4bb1211e0f41d063f7fdd31eba77a892f0
David Correa <latinoco@sudamericano.edu.ec>
2025-02-08 01:40:00 -0500

    Actualizar dependencias

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,11 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2020,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', '@nestjs/eslint-plugin'],
   extends: [
+    'plugin:@nestjs/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -15,11 +17,16 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist/**/*', 'build/**/*'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // Optional recommended rules (consider enabling these)
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'no-console': 'warn',
   },
 };
