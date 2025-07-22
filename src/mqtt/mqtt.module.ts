ef8cf5df245a3ffbb9138f0bf9d8b71eb38493c3
Ghostelkin <146010530+Elkin08c@users.noreply.github.com>
2025-07-21 19:27:06 -0500

    commit

--- src/mqtt/mqtt.module.ts
+++ src/mqtt/mqtt.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MqttService } from './mqtt.service';
 import { MqttController } from './mqtt.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { VitalSignsModule } from '../vital-signs/vital-signs.module'; // Importa el módulo de signos vitales
 
 @Module({
   imports: [
+    VitalSignsModule, // Importa el módulo de signos vitales
     ClientsModule.register([
       {
         name: 'MQTT_SERVICE',
--- src/vital-signs/vital-signs.module.ts
+++ src/vital-signs/vital-signs.module.ts
@@ -5,6 +5,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [VitalSignsController],
+  exports: [VitalSignsService],
   providers: [VitalSignsService, PrismaService],
 })
 export class VitalSignsModule {}
